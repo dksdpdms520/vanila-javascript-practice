f7fa42f02dc1a76de39341e914b92e9107aaddc8
YEN <dksdpdms520@gmail.com>
2022-03-28 11:10:37 +0900

    Add files via upload

---
+++ js-practice/score.js
@@ -0,0 +1,13 @@
+var scores = {
+    "kor": 55,
+    "mat": 75,
+    "eng": 50
+}
+
+if(scores['kor'] < 40 || scores['mat'] < 40 || scores['eng'] < 40 || (scores['kor'] + scores['mat'] + scores['eng'])/3 < 60 ){
+    document.write("fail");    
+}else{
+    document.write("pass");
+}
+
+// 시험 합격 여부
